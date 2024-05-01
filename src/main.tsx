6191a79376bd2c4ccebe03af8e5fa8456eeae7c4
C4ncino <193305@iberopuebla.mx>
2024-05-01 11:56:25 -0600

    add primereact provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import SessionContextProvider from '@context/SessionContext'
+import { PrimeReactProvider } from 'primereact/api';
 import '@styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <SessionContextProvider>
-      <RouterProvider router={router} />
-    </SessionContextProvider>
+    <PrimeReactProvider>
+      <SessionContextProvider>
+        <RouterProvider router={router} />
+      </SessionContextProvider>
+    </PrimeReactProvider>
   </React.StrictMode>,
 )
