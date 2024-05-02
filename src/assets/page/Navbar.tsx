e095ee7a00330a0a71e84b16a535bf8318134da7
C4ncino <193305@iberopuebla.mx>
2024-05-01 18:52:22 -0600

    configure primereact

--- src/assets/page/Navbar.tsx
+++ src/assets/page/Navbar.tsx
@@ -1,9 +1,35 @@
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+import { HomeIcon, TagIcon } from '@heroicons/react/24/outline';
+
 const NavBar = () => {
 
-    return (
-        <header>
+    const items: MenuItem[] = [
+        {
+            label: 'Home',
+            icon: <HomeIcon className='w-7 pe-1' />,
+            url: '/',
+        },
+        {
+            label: 'Products',
+            icon: <TagIcon className='w-7 pe-1' />,
+            url: '/products',
+        }
+    ]
 
-        </header>
+    {/* start={start} end={end} */ }
+    return (
+        <Menubar model={items}
+        // start={
+        //     <hgroup>
+        //         {/* TODO Logo */}
+        //         <h2 className='text-lg md:text-2xl'>E-Commerce</h2>
+        //     </hgroup>
+        // }
+        // end={
+        //     <></>
+        // }
+        />
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 import SessionContextProvider from '@context/SessionContext'
 import { PrimeReactProvider } from 'primereact/api';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 import '@styles/main.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <SessionContextProvider>
+    <SessionContextProvider>
+      <PrimeReactProvider value={{ appendTo: 'self' }}>
         <RouterProvider router={router} />
-      </SessionContextProvider>
-    </PrimeReactProvider>
-  </React.StrictMode>,
+      </PrimeReactProvider>
+    </SessionContextProvider>
+  </React.StrictMode >,
 )
