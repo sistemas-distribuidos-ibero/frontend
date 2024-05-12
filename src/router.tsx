669b51ff222373ec23b7e8a8b704c725282d3789
AlexLiCa <alexlin.carre@gmail.com>
2024-05-12 15:01:33 -0600

    A ver si esto se agrega

--- src/router.tsx
+++ src/router.tsx
@@ -9,47 +9,56 @@ import AdminCategories from "@pages/admin/AdminCategories";
 import Me from "@pages/Me";
 import Pay from "@pages/Pay";
 import Success from "@pages/cart/Success";
-
+import Orders from "@pages/orders/Orders";
+import CreateOrder from "@pages/orders/CreateOrder";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/signup",
-        element: <Signup />
-    },
-    {
-        path: "/products",
-        element: <Products />
-    },
-    {
-        path: "/me",
-        element: <Me />
-    },
-    {
-        path: "/products/:productId",
-        element: <ProductDetail />
-    },
-    {
-        path: "/admin/products",
-        element: <AdminProducts />
-    },
-    {
-        path: "/admin/categories",
-        element: <AdminCategories />
-    },
-    {
-        path: "/cart/pay",
-        element: <Pay />
-    },
-    {
-        path: "/cart/success",
-        element: <Success />
-    }
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/me",
+    element: <Me />,
+  },
+  {
+    path: "/products/:productId",
+    element: <ProductDetail />,
+  },
+  {
+    path: "/admin/products",
+    element: <AdminProducts />,
+  },
+  {
+    path: "/admin/categories",
+    element: <AdminCategories />,
+  },
+  {
+    path: "/cart/pay",
+    element: <Pay />,
+  },
+  {
+    path: "/cart/success",
+    element: <Success />,
+  },
+  {
+    path: "/orders",
+    element: <Orders />,
+  },
+  {
+    path: "/create_order",
+    element: <CreateOrder />,
+  },
 ]);
