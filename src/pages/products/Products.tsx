213e9f214c2d1fb8f8049b9499f40f6ada7d05d4
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2024-05-12 15:07:02 -0600

    Update Products.tsx

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,106 +1,68 @@
-import { useState, useEffect } from 'react';
-import PageTemplate from "@assets/PageTemplate";
-import { Product } from 'models/ProductModel';
-import CardProduct from './components/CardProduct';
-import { InputText } from 'primereact/inputtext';
+import { useEffect, useState } from 'react';
+import { DataScroller } from 'primereact/datascroller';
 import { Button } from 'primereact/button';
-import Filters from './components/Filters';
+import { Rating } from 'primereact/rating';
+import { Card } from "primereact/card";
+import PageTemplate from "@assets/PageTemplate";
+import { useProducts } from '@hooks/useProducts';
+import ProductDetail from "@pages/products/productdetail";
 
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    rating: number;
+    category: string;
+    image: string;
+};
 
 const Products = () => {
-    // const navigate = useNavigate();
-    const [products, setProducts] = useState<Product[]>([]);
-
+    const { product, fetchProducts } = useProducts();
+    const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+    const [showModal, setShowModal] = useState(false);
 
     useEffect(() => {
-        const products: Product[] = [
-            {
-                id: 1,
-                category: 1,
-                name: "Cámara Digital",
-                description: "Cámara de alta resolución 24MP, con capacidades de video 4K.",
-                stock: 10,
-                price: 350,
-                created: new Date(),
-                updated: new Date(),
-                image: 'camera.jpg',
-                rating: 0
-            },
-            {
-                id: 2,
-                category: 2,
-                name: "Smartphone",
-                description: "Último modelo, pantalla 6.5 pulgadas, 5G.",
-                stock: 2,
-                price: 999,
-                created: new Date(),
-                updated: new Date(),
-                image: './images/smartphone.jpg',
-                rating: 0
-            },
-            {
-                id: 3,
-                category: 1,
-                name: "Cámara Digital",
-                description: "Cámara de alta resolución 24MP, con capacidades de video 4K.",
-                stock: 10,
-                price: 350,
-                created: new Date(),
-                updated: new Date(),
-                image: 'camera.jpg',
-                rating: 0
-            },
-            {
-                id: 4,
-                category: 2,
-                name: "Smartphone",
-                description: "Último modelo, pantalla 6.5 pulgadas, 5G.",
-                stock: 2,
-                price: 999,
-                created: new Date(),
-                updated: new Date(),
-                image: './images/smartphone.jpg',
-                rating: 0
-            }
-        ];
-        setProducts(products);
-    }, []);
+        fetchProducts();
+    }, [fetchProducts]);
+
+    const handleProductSelect = (product) => {
+        setSelectedProduct(product);
+        setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedProduct(null);
+    };
+
+    const itemTemplate = (product) => (
+        <Card className="mx-3 my-2"  onClick={() => handleProductSelect(product)}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src={product.image} alt={product.name} style={{ width: '150px', height: 'auto', marginRight: '20px' }} />
+                <div>
+                    <h2>{product.name}</h2>
+                    <p>${product.price}</p>
+                    <div>
+                        <div className="text-700">{product.description}</div>
+                        <Rating value={product.rating} readOnly cancel={false} />
+                        <Button label="Add to Cart" icon="pi pi-shopping-cart" />
+                    </div>
+                </div>
+            </div>
+        </Card>
+    );
+
 
     return (
         <PageTemplate needBack2Top>
-            <>
-                {/* Buscador */}
-                <div className="p-inputgroup">
-                    <span className="p-inputgroup-addon">
-                        <Button icon="pi pi-search" className="p-button-rounded p-button-text border-2" />
-                    </span>
-                    <InputText placeholder=" Buscar..." className="p-inputtext-lg border-2 h-12" />
-                </div>
-
-                <div className="flex gap-3">
-                    {/* Filtros*/}
-                    <div className="w-1/4">
-
-                        <Filters />
-
-                    </div>
-
-                    {/* Productos */}
-                    <div className="gap-4">
-
-                        <div className="grid grid-cols-3 gap-4">
-                            {products.map(producto => (
-
-                                <CardProduct key={producto.id} producto={producto} />
-
-                            ))}
-                        </div>
-
-                    </div>
-                </div>
-            </>
+            <div className="card">
+                <DataScroller value={product} itemTemplate={itemTemplate} rows={5} inline scrollHeight="500px" header="Scroll Down to Load More" />
+                {showModal && <ProductDetail product={selectedProduct} onClose={handleCloseModal} />}
+            </div>
         </PageTemplate>
     );
 };
 
 export default Products;
+
