a25ab20ed0308cbbc91395471e5d1e1d79075a9a
az-rbl <bl.raul@outlook.com>
2024-05-12 22:49:17 -0600

    pay

--- src/pages/cart/Pay.tsx
+++ src/pages/cart/Pay.tsx
@@ -3,6 +3,35 @@ import PageTemplate from "@assets/page/PageTemplate";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
+
+
+
+const { post, delet} = useAPI();
+
+const addItemToCart = async (userId, itemId, quantity, token) => {
+    const endpoint = '/cart';
+    const body = JSON.stringify({ user_id: userId, item_id: itemId, quantity: quantity });
+    return await post(endpoint, token, body);
+}
+
+
+const fetchCart = async (userId, token) => {
+    const endpoint = '/get-cart';
+    const body = JSON.stringify({ user_id: userId });
+    return await post(endpoint, token, body);
+}
+
+
+const { delet } = useAPI();  // Note: Consider renaming 'delet' to 'delete' in your hook for better readability.
+
+const deleteCart = async (userId, token) => {
+    const endpoint = '/cart';
+    const body = JSON.stringify({ user_id: userId });
+    return await delet(endpoint, token, body);
+}
+
+
+
 const Pay = () => {
 
     const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
